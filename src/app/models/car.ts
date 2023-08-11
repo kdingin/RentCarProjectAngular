2afd02870022c3af7976257f39d7f22dc498955a
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2023-08-11 23:55:04 +0300

    .

---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car{
+    Id:number,
+    brandName:string,
+    colorName:string,
+    modelYear:number,
+    dailyPrice:number,
+    description:string,
+    carName:string
+}
---
+++ src/app/models/carDetail.ts
@@ -0,0 +1,10 @@
+export interface CarDetail{
+    Id:number,
+    brandName:string,
+    colorName:string,
+    dailyPrice:number,
+    description:string,
+    modelYear:number,
+    carName:string,
+    imagePath:string
+}
